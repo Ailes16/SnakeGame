9c04d46e33b3db4dd83655ae012edcd9c2953d53
Ailes16 <ailes.the16thclass@i.softbank.jp>
2022-09-08 21:33:45 +0900

    snakeクラスの実装

--- js/main.js
+++ js/main.js
@@ -1,8 +1,84 @@
+/* import */
+import {Snake} from './modules/snake.js';
+
+let mySnake;
+let game;
+let status = false;
+const timer = 10000/15;
+
 /* ゲーム画面の作成 */
-const canvas = document.createElement('canvas');
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 400;
-canvas.height = 400;
+/* 設定 */
+const GRID = 20;
+const STAGE = canvas.width / GRID;
 
-document.body.appendChild(canvas);
+/* 初期化 */
+const init = () => {
+    status = true;
+    game = setInterval(loop,timer);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    mySnake = new Snake(5,8);
+    console.log(mySnake.x,mySnake.y,mySnake.direction);
+}
+
+/* 繰り返し処理 */
+const loop = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    mySnake.move();
+    draw();
+    mySnake.judge(STAGE);
+    if(!mySnake.status){
+        end();
+    }
+    console.log(mySnake.x,mySnake.y,mySnake.direction);
+}
+
+/* ゲームエンド */
+const end = () => {
+    status = false;
+    clearInterval(game);
+}
+
+/* 描画 */
+const draw = () => {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(mySnake.x*GRID, mySnake.y*GRID, GRID-2, GRID-2);
+    for(let i = 0; i < mySnake.bx.length; i++){
+        ctx.fillRect(mySnake.bx[i]*GRID, mySnake.by[i]*GRID, GRID-2, GRID-2);
+    }
+}
+
+/* メイン処理 */
+init();
+
+/* キー操作 */
+document.addEventListener('keydown', e => {
+    if(status){
+        switch(e.key){
+            case 'ArrowLeft':
+                if(mySnake.direction !== 'R'){
+                    mySnake.direction = 'L';
+                }
+                break;
+            case 'ArrowRight':
+                if(mySnake.direction !== 'L'){
+                    mySnake.direction = 'R';
+                }
+                break;
+            case 'ArrowUp':
+                if(mySnake.direction !== 'D'){
+                    mySnake.direction = 'U';
+                }
+                break;
+            case 'ArrowDown':
+                if(mySnake.direction !== 'U'){
+                    mySnake.direction = 'D';
+                }
+                break;
+        }
+    }
+})
--- js/modules/snake.js
+++ js/modules/snake.js
@@ -1,8 +1,46 @@
-class Snake {
-    constructor(){
+export class Snake {
+    constructor(x, y, direction = 'R'){
 
-        this.x = null;
-        this.y = null;
-        this.direction = 'R';
+        this.x = x;
+        this.y = y;
+        this.bx = [];
+        this.by = [];
+        this.tail = 3;
+        this.direction = direction;
+        this.status = true;
+    }
+
+    move(){
+        if(this.bx.length < this.tail){
+            this.bx.push(this.x);
+            this.by.push(this.y);
+        }
+        for(let i = this.bx.length - 1; i > 0; i--){
+            this.bx[i] = this.bx[i - 1];
+            this.by[i] = this.by[i - 1];
+        }
+        this.bx[0] = this.x;
+        this.by[0] = this.y;
+
+        switch(this.direction){
+            case 'L':
+                this.x -= 1;
+                break;
+            case 'R':
+                this.x += 1;
+                break;
+            case 'U':
+                this.y -= 1;
+                break;
+            case 'D':
+                this.y += 1;
+                break;
+        }
+    }
+
+    judge(STAGE){
+        if(this.x < 0 || this.x > STAGE-1 || this.y < 0 || this.y > STAGE-1){
+            this.status = false;
+        }
     }
 }
